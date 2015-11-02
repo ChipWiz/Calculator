8e2f9a3d459a4cb41a673c97a5bfafb24f484511
ChipWiz <james.carrico@gmail.com>
2015-11-01 22:39:01 -0500

    adding the project

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by james carrico on 11/1/15.
+//  Copyright © 2015 james carrico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
